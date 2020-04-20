c141fe9cd24b83e451e1a094b8bcbf3c89dc9316
USER <user@USERnoMacBook-ea.local>
2020-04-20 14:31:34 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+// function hello(callback, lastname) {
+//     console.log(callback);
+//     console.log('hello' + callback(lastname));
+// }
+// function getName() {
+//     return 'Code Mafia';
+// }
+// const getFirstName = function () {
+//     return 'Code';
+// }
+// hello(function(name) {
+//     return 'Code' + name;
+// }, 'Mafia' );
+// hello(() => 'Code');
+
+
+function doSomething(a, b, callback) {
+    const result = callback(a, b);
+    console.log(result);
+}
+function multiply(a, b) {
+    return a * b;
+}
+function plus(a, b) {
+    return a + b;
+}
+doSomething(2, 2, multiply);
+doSomething(2, 3, plus);
